74616a78a353f425efa8d651bd03441bd25f2790
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-03-17 23:00:58 -0500

    Add test generating example biographical lineage report

--- tests/test_biography.py
+++ tests/test_biography.py
@@ -8,3 +8,8 @@ class BiographyTestCase(TestCase):
         biographer = Biographer("William-McCormick.ged")
         biography = biographer.generate_biography("Harry Glenn McCormick", dry_run=False)
         print(biography)
+
+    def test_lineage_report(self):
+        biographer = Biographer("William-McCormick.ged")
+        with open("lineage_report.txt", "w") as f:
+            biographer.write_lineage_report("William McCormick", "Jeremy Isaac McCormick", f)
