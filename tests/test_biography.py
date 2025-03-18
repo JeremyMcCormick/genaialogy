a23d95defae092e2fa2cb09d4c2e4b44cfaa9bb9
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-03-17 23:16:52 -0500

    Add biography dry run test

--- tests/test_biography.py
+++ tests/test_biography.py
@@ -9,6 +9,11 @@ class BiographyTestCase(TestCase):
         biography = biographer.generate_biography("Harry Glenn McCormick", dry_run=False)
         print(biography)
 
+    def test_biography_dry_run(self):
+        biographer = Biographer("William-McCormick.ged")
+        biography = biographer.generate_biography("Harry Glenn McCormick", dry_run=True)
+        print(biography)
+
     def test_lineage_report(self):
         biographer = Biographer("William-McCormick.ged")
         with open("lineage_report.txt", "w") as f:
